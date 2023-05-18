0fde0741189dce03dc2e51ff6eced4739ecc3617
Philip Uriarte <philipuriarte@gmail.com>
2023-05-18 22:29:40 +0800

    Add sidebar info

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import numpy as np
 
 
 with st.sidebar:
+    st.image()
     st.title("Pharmalytics")
     screen = st.radio("Navigation", ["Home", "Accounting Analytics", "Sales Prediction"])
+    st.info("Pharmalytics is a sales prediction application developed for FirstMed Pharmacy.")
 
 st.title("Sales Prediction System for Firstmed Pharmacy")
 st.write("Welcome to Pharmalytics!")
