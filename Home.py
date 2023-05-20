7d28344c2ed0414f86da134299c17ae88052d1e5
Philip Uriarte <philipuriarte@gmail.com>
2023-05-20 14:52:45 +0800

    Add improvements to pages and dataset saving

--- Home.py
+++ Home.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from streamlit_extras.app_logo import add_logo
+import os
 
 
 # Set page title and icon
@@ -13,10 +14,38 @@ st.set_page_config(
 add_logo("logo.png", height=10)
 
 # Main content
-st.title("Home")
-st.write("#### Pharmalytics is a sales prediction application developed for FirstMed Pharmacy.")
-file = st.file_uploader("Upload Pharmacy Sales Dataset")
+st.title("Welcome to Pharmalytics!")
+st.markdown(
+    """
+    Pharmalytics is a sales prediction system developed for FirstMed Pharmacy using the **SARIMA** model.
+    ### Instructions
+    1. Upload the pharmacy sales dataset below.
+    2.  👈 Select a Analytics or Predictions from the sidebar
+        - To gain insights into the dataset, click on the "Analytics" option. 
+        This will provide you with a comprehensive overview and analysis of the sales data, 
+        including key statistics, trends, and visualizations.
+        - To generate sales predictions, click on "Predictions" option.
+        Pharmalitics will employ the SARIMA model to generate sales predictions based on the uploaded dataset.
+"""
+)
+
+# Prompt to upload dataset
+dataset = None
+file = st.file_uploader("Upload Sales Dataset")
 if file:
     dataset = pd.read_csv(file, index_col=None)
-    st.write("Dataset Preview:")
+    st.success("Dataset uploaded successfully!")
+    st.write("**Dataset Preview:**")
     st.dataframe(dataset)
+
+    dataset.to_csv("uploaded_dataset.csv", index=None) # Save dataset to local machine
+
+# Load the previously uploaded dataset (if exists)
+uploaded_dataset = None
+if os.path.exists("uploaded_dataset.csv"):
+    uploaded_dataset = pd.read_csv("uploaded_dataset.csv", index_col=None)
+
+# Show the previously uploaded dataset
+if uploaded_dataset is not None and dataset is None:
+    st.write("**Previously Uploaded Dataset:**")
+    st.dataframe(uploaded_dataset)
--- pages/2_Sales_Predictions.py
+++ pages/2_Sales_Predictions.py
@@ -5,14 +5,14 @@ from streamlit_extras.app_logo import add_logo
 
 # Set page title and icon
 st.set_page_config(
-    page_title="Dataset Analytics",
-    page_icon="📈",
+    page_title="Sales Predictions",
+    page_icon="💰",
 )
 
 # Add the logo to the sidebar
 add_logo("logo.png", height=10)
 
 # Main content
-st.title("Dataset Analytics")
+st.title("Sales Predictions 💰")
 st.write("*option to open this screen will only show one a dataset file has been uploaded*")
-st.write("*will show sales data analytics user can enter input: choose from revenue/quantity of sales, per day/week/month/season and select specific day, month, etc.*")
+st.write("*will show sales predictions from trained SARIMA model*")
