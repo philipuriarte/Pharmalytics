602de1844bcf8c438b3e8bd70218e385f4c8d3d3
Philip Uriarte <philipuriarte@gmail.com>
2023-05-24 13:34:14 +0800

    Setup Sales Predictions

--- pages/2_Sales_Predictions.py
+++ pages/2_Sales_Predictions.py
@@ -1,7 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import os
+import statsmodels.api as sm
+from sklearn.model_selection import train_test_split
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 # Set page title and icon
 st.set_page_config(
@@ -18,15 +24,21 @@ st.markdown(
 """
 )
 
-# Load the previously uploaded dataset and stop if dataset doesn't exist
-uploaded_dataset = None
-if os.path.exists("uploaded_dataset.csv"):
-    uploaded_dataset = pd.read_csv("uploaded_dataset.csv", index_col=None)
+# Load the preprocessed dataset and stop if doesn't exist
+preprocessed_dataset = None
+if os.path.exists("preprocessed_dataset.csv"):
+    preprocessed_dataset = pd.read_csv("preprocessed_dataset.csv", parse_dates=["Date Sold"], index_col="Date Sold")
 else:
     st.warning("Dataset not uploaded. Please upload a dataset first in the Home page.")
     st.stop()
 
-st.write("Click the button below to generate sales predictions.")
+# Split the dataset into training and testing sets
+train_data, test_data = train_test_split(preprocessed_dataset, test_size=0.2, shuffle=False)
 
-if st.button("Generate", help="Click to generate sales predictions"):
-    pass
+# Show Train Set
+st.write("**Train Set**: ", train_data.shape[0])
+st.dataframe(train_data)
+
+# Show Test Set
+st.write("**Test Set**", test_data.shape[0])
+st.dataframe(test_data)
