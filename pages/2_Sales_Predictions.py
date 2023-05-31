fa09c5984270f2979f0d9c935ce169c0186bea29
Philip Uriarte <philipuriarte@gmail.com>
2023-05-31 19:45:11 +0800

    Add loading spinner and generate button

--- pages/2_Sales_Predictions.py
+++ pages/2_Sales_Predictions.py
@@ -120,105 +120,111 @@ with top_30_products_pred_con:
     # Input Widgets
     product_to_predict = st.selectbox("Select product to predict", unique_product_names)
     predict_interval = st.select_slider("Select time interval to predict", predict_time_intervals)
+    generate_button = st.button("Generate", help="Click to generate sales predictions")
 
-    # Get the data for the selected product
-    pred_product_data = preprocessed_dataset[preprocessed_dataset["Product Name"] == product_to_predict]
-    pred_resampled_data = pred_product_data.drop(["Product Name", "Sell Price", "Product Category"], axis=1).resample(time_interval).sum().reindex(date_range, fill_value=0).reset_index()
-    pred_resampled_data = pred_resampled_data.set_index("index")
-
-    # Split data into train and test sets
-    train_data, test_data = train_test_split(pred_resampled_data, test_size=0.2, shuffle=False)
-
-    # Use auto-SARIMA to determine the order and seasonal order
-    model = auto_arima(train_data['Quantity'], seasonal=True, m=4)
-    order = model.order
-    seasonal_order = model.seasonal_order
-
-    # Train the SARIMA model
-    sarima_model = sm.tsa.SARIMAX(train_data['Quantity'], order=order, seasonal_order=seasonal_order)
-    sarima_model_fit = sarima_model.fit()
-
-    # Generate predictions on the test set
-    predictions = sarima_model_fit.predict(start=test_data.index[0], end=test_data.index[-1])
-
-    # Calculate accuracy statistics
-    mae = mean_absolute_error(test_data['Quantity'], predictions)
-    mse = mean_squared_error(test_data['Quantity'], predictions)
-    rmse = np.sqrt(mse)
-
-    act_pred_tab, final_app_tab = st.tabs(["📒 Actual vs Predicted", "📊 Final App"])
-
-    col1, col2 = st.columns(2)
-
-    with act_pred_tab:
-        data = pd.concat([pred_resampled_data['Quantity'].rename('Actual'), predictions.rename('Predicted')], axis=1).reset_index()
-
-        chart = alt.Chart(data).mark_line().encode(
-            x='index:T',
-            y=alt.Y('value:Q', axis=alt.Axis(title='Quantity')),
-            color=alt.Color('data:N', scale=alt.Scale(domain=['Actual', 'Predicted'], range=['steelblue', 'orange'])),
-            tooltip=['index:T', 'value:Q', 'data:N']
-        ).transform_fold(
-            fold=['Actual', 'Predicted'],
-            as_=['data', 'value']
-        ).properties(
-            title="Actual vs Predicted",
-            width=600,
-            height=400
-        )
-
-        st.altair_chart(chart)
+    if generate_button is not True:
+        st.stop()
     
-    with final_app_tab:
-        data = pd.concat([train_data['Quantity'].rename('Actual'), predictions.rename('Predicted')], axis=1).reset_index()
+    with st.spinner('Processing...'):
+        # Get the data for the selected product
+        pred_product_data = preprocessed_dataset[preprocessed_dataset["Product Name"] == product_to_predict]
+        pred_resampled_data = pred_product_data.drop(["Product Name", "Sell Price", "Product Category"], axis=1).resample(time_interval).sum().reindex(date_range, fill_value=0).reset_index()
+        pred_resampled_data = pred_resampled_data.set_index("index")
 
-        chart = alt.Chart(data).mark_line().encode(
-            x='index:T',
-            y=alt.Y('value:Q', axis=alt.Axis(title='Quantity')),
-            color=alt.Color('data:N', scale=alt.Scale(domain=['Actual', 'Predicted'], range=['steelblue', 'orange'])),
-            tooltip=['index:T', 'value:Q', 'data:N']
-        ).transform_fold(
-            fold=['Actual', 'Predicted'],
-            as_=['data', 'value']
-        ).properties(
-            title="What the predictions should look like in final app",
-            width=600,
-            height=400
-        )
+        # Split data into train and test sets
+        train_data, test_data = train_test_split(pred_resampled_data, test_size=0.2, shuffle=False)
 
-        st.altair_chart(chart)
-    
-    col1, col2 = st.columns(2)
-    with col1:
-        st.write("**Auto-ARIMA parameters**")
-        st.write("Order: ", order)
-        st.write("Seasonal Order: ", seasonal_order)
-    with col2:        
-        st.write("**Accuracy Results**")
-        st.write("MAE: ", mae)
-        st.write("RMSE: ", rmse)
+        # Use auto-SARIMA to determine the order and seasonal order
+        model = auto_arima(train_data['Quantity'], seasonal=True, m=4)
+        order = model.order
+        seasonal_order = model.seasonal_order
 
-    extra_info_expander = st.expander("See Extra Information")
-    with extra_info_expander:
-        st.write("**Product Sales Aggregated Dataset**")
-        st.dataframe(pred_resampled_data)
-        
-        # Perform ACF and PACF analysis
-        fig, ax = plt.subplots(2, 1, figsize=(10, 8))
-        plot_acf(pred_resampled_data['Quantity'], lags=20, ax=ax[0])
-        plot_pacf(pred_resampled_data['Quantity'], lags=10, ax=ax[1])
-        ax[0].set_title(f"ACF Plot - {product_to_predict}")
-        ax[1].set_title(f"PACF Plot - {product_to_predict}")
-        plt.tight_layout()
-        st.write("**ACF and PCF Plot**")
-        st.pyplot(fig)
+        # Train the SARIMA model
+        sarima_model = sm.tsa.SARIMAX(train_data['Quantity'], order=order, seasonal_order=seasonal_order)
+        sarima_model_fit = sarima_model.fit()
+
+        # Generate predictions on the test set
+        predictions = sarima_model_fit.predict(start=test_data.index[0], end=test_data.index[-1])
+
+        # Calculate accuracy statistics
+        mae = mean_absolute_error(test_data['Quantity'], predictions)
+        mse = mean_squared_error(test_data['Quantity'], predictions)
+        rmse = np.sqrt(mse)
+
+        act_pred_tab, final_app_tab = st.tabs(["📒 Actual vs Predicted", "📊 Final App"])
 
         col1, col2 = st.columns(2)
+
+        with act_pred_tab:
+            data = pd.concat([pred_resampled_data['Quantity'].rename('Actual'), predictions.rename('Predicted')], axis=1).reset_index()
+
+            chart = alt.Chart(data).mark_line().encode(
+                x='index:T',
+                y=alt.Y('value:Q', axis=alt.Axis(title='Quantity')),
+                color=alt.Color('data:N', scale=alt.Scale(domain=['Actual', 'Predicted'], range=['steelblue', 'orange'])),
+                tooltip=['index:T', 'value:Q', 'data:N']
+            ).transform_fold(
+                fold=['Actual', 'Predicted'],
+                as_=['data', 'value']
+            ).properties(
+                title="Actual vs Predicted",
+                width=600,
+                height=400
+            )
+
+            st.altair_chart(chart)
         
+        with final_app_tab:
+            data = pd.concat([train_data['Quantity'].rename('Actual'), predictions.rename('Predicted')], axis=1).reset_index()
+
+            chart = alt.Chart(data).mark_line().encode(
+                x='index:T',
+                y=alt.Y('value:Q', axis=alt.Axis(title='Quantity')),
+                color=alt.Color('data:N', scale=alt.Scale(domain=['Actual', 'Predicted'], range=['steelblue', 'orange'])),
+                tooltip=['index:T', 'value:Q', 'data:N']
+            ).transform_fold(
+                fold=['Actual', 'Predicted'],
+                as_=['data', 'value']
+            ).properties(
+                title="What the predictions should look like in final app",
+                width=600,
+                height=400
+            )
+
+            st.altair_chart(chart)
+        
+        col1, col2 = st.columns(2)
         with col1:
-            st.write("Train Set")
-            st.dataframe(train_data)
-        with col2:
-            st.write("Test Set")
-            st.dataframe(test_data)
+            st.write("**Auto-ARIMA parameters**")
+            st.write("Order: ", order)
+            st.write("Seasonal Order: ", seasonal_order)
+        with col2:        
+            st.write("**Accuracy Results**")
+            st.write("MAE: ", mae)
+            st.write("RMSE: ", rmse)
+
+        extra_info_expander = st.expander("See Extra Information")
+        with extra_info_expander:
+            st.write("**Product Sales Aggregated Dataset**")
+            st.write(product_to_predict)
+            st.dataframe(pred_resampled_data)
+            
+            # Perform ACF and PACF analysis
+            fig, ax = plt.subplots(2, 1, figsize=(10, 8))
+            plot_acf(pred_resampled_data['Quantity'], lags=20, ax=ax[0])
+            plot_pacf(pred_resampled_data['Quantity'], lags=10, ax=ax[1])
+            ax[0].set_title(f"ACF Plot - {product_to_predict}")
+            ax[1].set_title(f"PACF Plot - {product_to_predict}")
+            plt.tight_layout()
+            st.write("**ACF and PCF Plot**")
+            st.pyplot(fig)
+
+            col1, col2 = st.columns(2)
+            
+            with col1:
+                st.write("Train Set")
+                st.dataframe(train_data)
+            with col2:
+                st.write("Test Set")
+                st.dataframe(test_data)
 
