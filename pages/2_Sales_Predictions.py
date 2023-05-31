2388982ac653181fbba63d6ccefedd54e717c3bc
Philip Uriarte <philipuriarte@gmail.com>
2023-05-31 18:51:23 +0800

    Remove plt import

--- pages/2_Sales_Predictions.py
+++ pages/2_Sales_Predictions.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import os
 import statsmodels.api as sm
 from sklearn.model_selection import train_test_split
